731d7cc6e4f7a701bf54e6e7344b64752663dac5
Alexandre Faustino <alexmigf@gmail.com>
2025-01-08 12:04:52 +0000

    Update wpo-ips-xrechnung.php

--- wpo-ips-xrechnung.php
+++ wpo-ips-xrechnung.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'WPO_IPS_XRechnung' ) ) {
 		 *
 		 * @var string
 		 */
-		public $base_plugin_version = '3.9.5-beta-6';
+		public $base_plugin_version = '3.9.5-beta-7';
 		
 		/**
 		 * UBL format
