7b19277f4bc80e5b51fddc154d0c9f92c69f9591
Alexandre Faustino <alexmigf@gmail.com>
2025-01-10 18:53:10 +0000

    v1.0.3-beta-6

--- wpo-ips-xrechnung.php
+++ wpo-ips-xrechnung.php
@@ -4,7 +4,7 @@
  * Requires Plugins: woocommerce-pdf-invoices-packing-slips
  * Plugin URI:       https://github.com/wpovernight/wpo-ips-xrechnung
  * Description:      XRechnung add-on for PDF Invoices & Packing Slips for WooCommerce plugin.
- * Version:          1.0.3-beta-5
+ * Version:          1.0.3-beta-6
  * Update URI:       https://github.com/wpovernight/wpo-ips-xrechnung
  * Author:           WP Overnight
  * Author URI:       https://wpovernight.com
@@ -26,14 +26,14 @@ if ( ! class_exists( 'WPO_IPS_XRechnung' ) ) {
 		 *
 		 * @var string
 		 */
-		public $version = '1.0.3-beta-5';
+		public $version = '1.0.3-beta-6';
 		
 		/**
 		 * Base plugin version
 		 *
 		 * @var string
 		 */
-		public $base_plugin_version = '3.9.5-beta-8';
+		public $base_plugin_version = '3.9.5-beta-9';
 		
 		/**
 		 * UBL format
